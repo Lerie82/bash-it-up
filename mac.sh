a15a0d2177fdae54d13d000a6b17fdff7d28cad4
Lerie Taylor <ltaylor82619@gmail.com>
2020-10-15 09:10:08 -0700

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -2,7 +2,8 @@
 sudo ifconfig wlan0 down
 sleep 1
 
-sudo ifconfig wlan0 hw ether 10:7B:44:64:FA:57
+#sudo ifconfig wlan0 hw ether 10:7B:44:64:FA:57
+sudo ifconfig wlan0 hw ether 02:22:33:44:55
 sleep 3
 
 sudo ifconfig wlan0 up
