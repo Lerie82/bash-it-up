60ff6eb1b34293cbd35d4ef8c3ce63e1c9fd1543
Lerie Taylor <ltaylor82619@gmail.com>
2020-10-15 09:44:25 -0700

    Update android.sh

--- android.sh
+++ android.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+git clone https://gitlab.com/BenjaminDobell/Heimdall.git
+
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0BB4A1B2FA1A38EB
 sudo add-apt-repository "deb http://ppa.launchpad.net/samoilov-lex/aftl-stable/ubuntu artful main"
 sudo apt update
 
-sudo apt -y -s install wget android-tools-fastboot android-tools-adb android-sdk-platform-tools-common android-tools-fsutils
+sudo apt -y -s install wget android-tools-fastboot android-tools-adb android-sdk-platform-tools-common android-tools-fsutils  libusb-1.0-0-dev
 wget https://github.com/masterdevx/termux-adb/raw/master/InstallTools.sh -q
