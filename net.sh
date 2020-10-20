f39b88e4ffb9d3714bc9a04d311beab7cbd542cb
lerie82 <ltaylor82619@gmail.com>
2020-10-19 19:32:17 -0700

    next...

--- net.sh
+++ net.sh
@@ -1,4 +1,9 @@
 #/bin/bash
 
 # download some networking tools
-sudo apt -y install tcpreplay finger iodine tshark tcpdump
+sudo apt -y install tcpreplay finger iodine tshark tcpdump network-manager wireless-tools
+
+# network-manager
+sudo service network-manager start
+#sudo systemctl start NetworkManager.service
+#sudo systemctl enable NetworkManager.service
