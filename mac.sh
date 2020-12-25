9b06ab62916a64c2c19ecbe0c7e93783990bdc79
lerie82 <ltaylor82619@gmail.com>
2020-12-25 03:27:11 -0700

    party on wayne..

--- mac.sh
+++ mac.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 sudo ifconfig wlan0 down
+printf "working."
 sleep 1
 
 #sudo ifconfig wlan0 hw ether 10:7B:44:64:FA:57
-sudo ifconfig wlan0 hw ether 02:22:33:44:55
-sleep 3
+sudo ifconfig wlan0 hw ether 02:11:22:33:44:55
+printf ".."
+sleep 5
 
 sudo ifconfig wlan0 up
+printf "..."
+sleep 10
 
+curl -v -L "http://gstatic.com/generate_204"
